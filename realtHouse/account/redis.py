4c083c50ac03b71bf6dea643823e217b698932c3
foxtrot983 <wladiksan@gmail.com>
2023-06-01 12:24:28 +0300

    all done

--- realtHouse/account/redis.py
+++
@@ -1,6 +0,0 @@
-import redis
-from django.conf import settings
-
-def redis_instance():
-    redis_instance = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
-    return redis_instance
